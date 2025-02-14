cff940e6df81187f84da33109ee97f462c4e868d
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2025-02-15 01:34:02 +0900

    멀티 프로세싱

--- polaris.py
+++ polaris.py
@@ -1,6 +1,5 @@
 # 프로젝트 폴라리스 - 추적 매매 시스템
 import asyncio
-import time
 
 
 async def slot1_main():
@@ -12,13 +11,13 @@ async def slot1_main():
 async def slot2_main():
     while True:
         print('Slot2 main loop start')
-        await asyncio.sleep(3)
+        await asyncio.sleep(1.5)
         print('Slot2 main loop stop')
 
 async def slot3_main():
     while True:
         print('Slot3 main loop start')
-        await asyncio.sleep(1)
+        await asyncio.sleep(1.8)
         print('Slot3 main loop stop')
 
 async def slot4_main():
@@ -28,8 +27,14 @@ async def slot4_main():
         print('Slot4 main loop stop')
 
 async def main():
-    await asyncio.gather(slot1_main(), slot2_main(), slot3_main(), slot4_main())
+    asyncio.create_task(slot1_main())
+    asyncio.create_task(slot2_main())
+    asyncio.create_task(slot3_main())
+    asyncio.create_task(slot4_main())
+
+    while True:
+        await asyncio.sleep(1)
+
 
 asyncio.run(main())
-
 
